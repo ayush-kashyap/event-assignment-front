8de6a6cfd8c9d65670ba4f333cd55c22fcc2007a
Ayush Kashyap <78103946+ayush-kashyap@users.noreply.github.com>
2026-01-19 13:32:06 +0530

    Fixed Https Issue

--- src/components/appBase.js
+++ src/components/appBase.js
@@ -16,7 +16,7 @@ export const AppBase=()=>{
 
     const getEvents=()=>{
         setDataLoading(true);
-        axios.get("http://event-assignment-backend.vercel.app/api/event/events").then((res)=>{
+        axios.get("https://event-assignment-backend.vercel.app/api/event/events").then((res)=>{
             dispatch(setEvents(res.data));
             setDataLoading(false);
         }).catch((err)=>{
@@ -26,7 +26,7 @@ export const AppBase=()=>{
     }
     const bookTicket=(id)=>{
         setDataLoading(true);
-        axios.post(`http://event-assignment-backend.vercel.app/api/ticket/book/${id}`).then((res)=>{
+        axios.post(`https://event-assignment-backend.vercel.app/api/ticket/book/${id}`).then((res)=>{
             message.success(res.data);
             getEvents();
             setDataLoading(false);
